abec828b5b892c3b9c003f8b73503725ccaf464e
raj-upadhyay <rrajupadhyayy@gmail.com>
2022-06-17 18:11:25 +0530

    Project setup ( first commit )

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Testing</Text>
+    </View>
+  );
+}
+
+export default App;
